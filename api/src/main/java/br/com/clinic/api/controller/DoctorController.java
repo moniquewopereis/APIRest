172879c70bcc32a7394b29760d486ea433c98be0
Monique Candido Wopereis <mcwopereis@yahoo.com.br>
2023-05-26 21:05:00 +0100

    Creating the first Controller with Post Method using Insomnia as a test tool

---
+++ api/src/main/java/br/com/clinic/api/controller/DoctorController.java
@@ -0,0 +1,17 @@
+package br.com.clinic.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+	
+	@PostMapping	
+	public void register(@RequestBody String json) {
+		System.out.println(json);
+	}
+
+}
